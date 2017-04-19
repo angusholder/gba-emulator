a76c74f54acadc9bb1efad57faeb8aa0a0cd8bfb
Angus Holder <aholder97@gmail.com>
2017-04-19 09:38:00 +0100

    The planned optimised dispatch code for thumb instructions is now moved to the gist for issue #1.

--- src/arm7tdmi/thumb_core.rs
+++ src/arm7tdmi/thumb_core.rs
@@ -14,42 +14,6 @@ pub fn step_thumb(arm: &mut Arm7TDMI, interconnect: &mut Interconnect, op: u16) 
     THUMB_LUT[discr](arm, interconnect, op)
 }
 
-/*
-#[inline(always)]
-fn dispatch_thumb_fast(arm: &mut Arm7TDMI, interconnect: &mut Interconnect, mut cycles_remaining: i32) -> i32 {
-    if cycles_remaining > 0 {
-        let pc = arm.regs[REG_PC] + 2;
-        if let Some(preload) = interconnect.exec16_fast(pc, Some(&mut cycles_remaining)) {
-            arm.regs[REG_PC] = pc;
-
-            let next_op = interconnect.prefetch[0] as u16;
-            interconnect.prefetch[0] = interconnect.prefetch[1];
-            interconnect.prefetch[1] = preload as u32;
-
-            THUMB_LUT[(next_op >> 6) as usize](arm, interconnect, next_op, cycles_remaining)
-        } else {
-            #[cold]dispatch_thumb_slow(arm, interconnect, cycles_remaining)
-        }
-    } else {
-        cycles_remaining
-    }
-}
-
-#[cold]
-#[inline(never)]
-fn dispatch_thumb_slow(arm: &mut Arm7TDMI, interconnect: &mut Interconnect, mut cycles_remaining: i32) -> i32 {
-    if cycles_remaining > 0 {
-        let pc = arm.regs[REG_PC] + 2;
-        let next_op = interconnect.prefetch[0] as u16;
-        interconnect.prefetch[0] = interconnect.prefetch[1];
-        interconnect.prefetch[1] = interconnect.exec16_slow(pc, Some(&mut cycles_remaining)) as u32;
-        arm.regs[REG_PC] = pc;
-        THUMB_LUT[(next_op >> 6) as usize](arm, interconnect, next_op, cycles_remaining)
-    } else {
-        cycles_remaining
-    }
-}*/
-
 type ThumbOp = fn(arm: &mut Arm7TDMI, interconnect: &mut Interconnect, op: u16) -> StepEvent;
 
 include!(concat!(env!("OUT_DIR"), "/thumb_core_generated.rs"));
