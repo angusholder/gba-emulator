ebcb1a22de463304cff4dca5ad0f74da2321101c
angusholder <aholder97@gmail.com>
2018-08-15 01:43:31 +0100

    Delete Python codegen for thumb core

--- src/gen.py
+++ src/gen.py
@@ -1,7 +1,7 @@
 import argparse
 import os
 
-from arm7tdmi import arm_core_gen, thumb_core_gen
+from arm7tdmi import arm_core_gen
 
 parser = argparse.ArgumentParser(description='Generate sources for CPU cores.')
 parser.add_argument('--out-dir', help='set output directory')
@@ -9,5 +9,4 @@ parser.add_argument('--out-dir', help='set output directory')
 args = parser.parse_args()
 out_dir = args.out_dir
 
-thumb_core_gen.generate(os.path.join(out_dir, 'thumb_core_generated.rs'))
 arm_core_gen.generate(os.path.join(out_dir, 'arm_core_generated.rs'))
